2db8d6e73c96fe0dd25d51eecf44416f812056d4
kornrullez <kevinlutgert@MacBook-van-Kevin.local>
2014-12-09 19:14:00 +0100

    v2.1
    v 2.1

--- app/src/main/java/com/example/kevinlutgert/materialdesign/MainActivity.java
+++ app/src/main/java/com/example/kevinlutgert/materialdesign/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.kevinlutgert.fooi;
+package com.example.kevinlutgert.materialdesign;
 
 import android.app.Activity;
 import android.content.SharedPreferences;
@@ -9,14 +9,18 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.view.inputmethod.EditorInfo;
 import android.widget.TextView.OnEditorActionListener;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
 import java.text.DecimalFormat;
 
 
-public class MainActivity extends Activity implements OnEditorActionListener, OnClickListener {
+public class MainActivity extends Activity implements OnEditorActionListener, OnCheckedChangeListener, OnSeekBarChangeListener {
     private EditText rekeningBedragEditText;
     private TextView procentTextView;
     private Button procentPlusButton;
@@ -25,12 +29,15 @@ public class MainActivity extends Activity implements OnEditorActionListener, On
     private TextView totaalTextView;
     private double fooiPercentage = 15.0;
     private SharedPreferences bewaardeWaardes;
-    private RadioButton radioBoptie1;
-    private RadioButton radioBoptie2;
-    private RadioButton radioBoptie3;
-
+    private RadioGroup afrondRadioGroup;
+    private SeekBar  seekbar;
     private String rekeningBedragString = "";
     private int fooiPercentageInt;
+
+    private final int AFROND_GEEN = 0;
+    private final int AFROND_FOOI = 1;
+    private final int AFROND_TOTAAL = 2;
+    private int afronding = AFROND_GEEN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,32 +47,21 @@ public class MainActivity extends Activity implements OnEditorActionListener, On
 
         rekeningBedragEditText = (EditText) findViewById(R.id.rekeningBedragEditText);
         procentTextView = (TextView) findViewById(R.id.ProcentView);
-        procentPlusButton = (Button) findViewById(R.id.procent_PlusButton);
-        procentMinButton = (Button) findViewById(R.id.procent_MinButton);
+        seekbar = (SeekBar) findViewById(R.id.procentSeekBar);
+        seekbar.setOnSeekBarChangeListener(this);
         fooiTextView = (TextView) findViewById(R.id.ProcentTotaal);
         totaalTextView = (TextView) findViewById(R.id.Totaal);
-        radioBoptie1  =(RadioButton) findViewById(R.id.optie1RadioButton);
-        radioBoptie2 = (RadioButton) findViewById(R.id.optie2RadioButton);
-        radioBoptie3 = (RadioButton) findViewById(R.id.optie3RadioButton);
-
         rekeningBedragEditText.setOnEditorActionListener(this);
-        procentMinButton.setOnClickListener(this);
-        procentPlusButton.setOnClickListener(this);
+        afrondRadioGroup = (RadioGroup) findViewById(R.id.radiogroep);
+        afrondRadioGroup.setOnCheckedChangeListener(this);
 
     }
 
-    void setChecked()
-    radioBoption1.setChecked(true);
-
-boolean isChecked()
-    if (optie2RadioButton.isChecked) {
-        //Doe iets
-    }
     @Override
     public void onPause() {
         // bewaar de variabelen
         SharedPreferences.Editor editor = bewaardeWaardes.edit();
-        editor.putString("rekeningBedragString", rekeningBedragString);
+        editor.putString("rekeningBedragString", rekeningBedragEditText.getText().toString());
         fooiPercentageInt = (int) fooiPercentage;
         editor.putInt("fooiPercentage", fooiPercentageInt);
         editor.commit();
@@ -88,20 +84,6 @@ boolean isChecked()
         berekenEnToon();
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.procent_MinButton:
-                fooiPercentage--;
-                berekenEnToon();
-                break;
-            case R.id.procent_PlusButton:
-                fooiPercentage++;
-                berekenEnToon();
-                break;
-        }
-
-    }
 
     @Override
     public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
@@ -126,16 +108,56 @@ boolean isChecked()
         //Bereken fooi en totaal//
 
         double fooibedrag = rekeningBedrag * (fooiPercentage / 100);
+        if (afronding == AFROND_FOOI) {
+            fooibedrag = Math.round(fooibedrag);
+        }
         double totaalbedrag = rekeningBedrag + fooibedrag;
+        if (afronding == AFROND_TOTAAL) {
+            totaalbedrag = Math.round(totaalbedrag);
+            fooibedrag = totaalbedrag - rekeningBedrag;
+        }
 
         DecimalFormat df = new DecimalFormat("€ ###,###.00");
         fooiTextView.setText(df.format(fooibedrag));
         totaalTextView.setText(df.format(totaalbedrag));
         procentTextView.setText("" + (int) fooiPercentage + "%");
+
+
     }
 
 
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        switch (checkedId) {
+            case R.id.optie1RadioButton:
+                afronding = AFROND_GEEN;
+                break;
+            case R.id.optie2RadioButton:
+                afronding = AFROND_FOOI;
+                break;
+            case R.id.optie3RadioButton:
+                afronding = AFROND_TOTAAL;
+        }
+        berekenEnToon();
+    }
 
+    //doet niets
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+    }
+
+    //verandert alleen fooiPercentageTextview
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        fooiPercentageInt = i;
+        fooiPercentage = (double) fooiPercentageInt;
+        procentTextView.setText("" + fooiPercentageInt + "%");
+    }
+
+    //maak de berekening na input gebruiker
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        berekenEnToon();
+    }
 }
-
 
